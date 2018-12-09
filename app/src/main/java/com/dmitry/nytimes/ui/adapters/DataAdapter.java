a901caa3e6a85a0585b86a2500aae39c2f16aabb
Dmitry <dim@uniphone.biz>
2018-12-09 21:07:22 +0200

    add Glide for load images

--- app/src/main/java/com/dmitry/nytimes/ui/adapters/DataAdapter.java
+++ app/src/main/java/com/dmitry/nytimes/ui/adapters/DataAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
 import com.dmitry.nytimes.R;
 import com.dmitry.nytimes.models.Title;
 
@@ -14,14 +15,14 @@ import java.util.ArrayList;
 public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
 
     private ArrayList<Title> resultTitles;
-
+    private View view;
     public DataAdapter(ArrayList<Title> resultTitles) {
         this.resultTitles = resultTitles;
     }
 
     @Override
     public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_title, parent, false);
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_title, parent, false);
         return new ViewHolder(view);
     }
 
@@ -29,6 +30,10 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
     public void onBindViewHolder(DataAdapter.ViewHolder holder, int position) {
         final String urlCurrentArticle = resultTitles.get(position).getUrl();
         holder.title.setText(resultTitles.get(position).getTitle());
+        Glide
+                .with(view.getContext())
+                .load(resultTitles.get(position).getMedia().get(0).getMediaMedia().get(1).getUrl())
+                .into(holder.image);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,8 +53,8 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
 
         ViewHolder(View itemView) {
             super(itemView);
-            title = (TextView) itemView.findViewById(R.id.title_on_card);
-
+            title = itemView.findViewById(R.id.title_on_card);
+            image = itemView.findViewById(R.id.img_on_card);
         }
     }
 }
